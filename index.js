70955daedf0849ba988f75ced7db7e42d650ec22
Gaddict <ksano6253@gmail.com>
2016-01-27 01:28:43 +0900

    crowl

---
+++ index.js
@@ -0,0 +1,71 @@
+//Initialize instance
+var sequelize = require("sequelize");
+ 
+//Connect to database
+var connection = new sequelize('データベース名', 'ユーザ名', 'パスワード', {オプション: '値'});
+ 
+// Define models
+var sites = connection.define("sites", {
+    url: sequelize.TEXT,
+    title: sequelize.STRING
+});
+ 
+// ----
+ 
+//Initialize instance
+var request = require("request");
+var cheerio = require("cheerio");
+ 
+// ----
+ 
+// Define request url
+var requestUrl = "http://www.google.com";
+ 
+// Send http request
+request({url: requestUrl}, function(error, response, body) {
+ 
+    // If request succeed
+    if (!error && response.statusCode == 200) {
+        $ = cheerio.load(body); // Create cheerio instance
+ 
+        // Get response data
+        var url = response.request.href;
+        var title = $("title").text();
+ 
+        console.log(url);
+        console.log(title);
+ 
+        // Create new instance
+        var site = sites.build();
+ 
+        // Set fields
+        site.url = url;
+        site.title = title;
+ 
+        // Save to database
+        site.save()
+            .success(function(anotherTask) {
+                console.log('Succeed');
+            })
+            .error(function(error) {
+                console.log(error);
+            });
+    }
+ 
+    // If error occured
+    else {
+        console.log("--------------------------------------------------");
+        if (error && "code" in error) {
+            console.log("Error Code:" + error.code);
+        }
+        if (error && "errno" in error) {
+            console.log("Error No:" + error.errno);
+        }
+        if (error && "syscall" in error) {
+            console.log("Error Syscall:" + error.syscall);
+        }
+        if (response && "statusCode" in response) {
+            console.log("Status Code:" +  response.statusCode);
+        }
+    }
+});
