3a9b6e6f6be3288220a2779935a1668f13399159
Gaddict <ksano6253@gmail.com>
2016-04-22 00:04:30 +0900

    fix format

--- start.js
+++ start.js
@@ -1,36 +1,36 @@
-var update_user_list = require('./user_list.js');
-var sleep = require('sleep');
+var update_user_list = require('./user_list.js')
+var sleep = require('sleep')
 
-//Initialize instance
-var Sequelize = require("sequelize");
+// Initialize instance
+var Sequelize = require('sequelize')
 
-//Connect to database
-var connection = new Sequelize('stock_forecast', 'root', 'password',{} );
+// Connect to database
+var connection = new Sequelize('stock_forecast', 'root', 'password', {})
 
 // Define models
 var users = connection.define('users', {
-    user_id: { type: Sequelize.STRING(4), allowNull: false}
-});
+  user_id: {type: Sequelize.STRING(4), allowNull: false}
+})
 
 // Define models
 var forecasts = connection.define('forecasts', {
-    user_id: {
-      type: Sequelize.STRING(4),
-      allowNull: false
-    },
-    win: { type: Sequelize.INTEGER }, // 1:勝ち、0:負け、9:予想中
-    start_date: { type: Sequelize.DATE },
-    position: { type: Sequelize.BOOLEAN }, // 1:買い、0:売り
-    span: { type: Sequelize.INTEGER }, // 1:1日、7:１週、30:１ヶ月
-    stock_code: {
-      type: Sequelize.STRING(4),
-      allowNull: false
-    },
-    start_price: { type: Sequelize.INTEGER },
-    end_price: { type: Sequelize.INTEGER },
-    highest_price: { type: Sequelize.INTEGER },
-    lowest_price: { type: Sequelize.INTEGER }
-});
+  user_id: {
+    type: Sequelize.STRING(4),
+    allowNull: false
+  },
+  win: { type: Sequelize.INTEGER }, // 1:勝ち、0:負け、9:予想中
+  start_date: { type: Sequelize.DATE },
+  position: { type: Sequelize.BOOLEAN }, // 1:買い、0:売り
+  span: { type: Sequelize.INTEGER }, // 1:1日、7:１週、30:１ヶ月
+  stock_code: {
+    type: Sequelize.STRING(4),
+    allowNull: false
+  },
+  start_price: { type: Sequelize.INTEGER },
+  end_price: { type: Sequelize.INTEGER },
+  highest_price: { type: Sequelize.INTEGER },
+  lowest_price: { type: Sequelize.INTEGER }
+})
 
 // cronJob = require('cron').CronJob
 //
